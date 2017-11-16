63ac33a1feeab395714fd74908aa8394fe12e61d
ElCapitanAsul <cmanero@uic.edu.ph>
2017-11-16 19:02:56 +0800

    updated singupcustomer page

--- src/pages/signup-customer/signup-customer.ts
+++ src/pages/signup-customer/signup-customer.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Http } from '@angular/http';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { VendorsListPage } from '../vendors-list/vendors-list';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { HttpErrorResponse } from '@angular/common/http';
 
@@ -17,12 +18,82 @@ import { HttpErrorResponse } from '@angular/common/http';
   templateUrl: 'signup-customer.html',
 })
 export class SignupCustomerPage {
+	loading: string;
+	fname: string;
+	lname: string;
+	email: string;
+	password: string;
+	result: string;
+	vendors: Array<{
+    fname: string, 
+    lname: string,
+    email: string,
+    rating: string,
+    }>;
+	
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+  	public navCtrl: NavController, 
+  	public navParams: NavParams,
+  	public http: Http,
+    public alertCtrl: AlertController
+  	) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignupCustomerPage');
+  }
+
+  attemptClient(){
+    this.loading = 'Loading...';
+    // this.http.setHeader('Content-Type', 'application/json');
+    // this.http.get('https://restcountries.eu/rest/v2/name/eesti')
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded;');
+
+    let value = { "fname": this.fname, "lname": this.lname, "email": this.email, "password": this.password};
+    // let body:string = 'email=' + this.email + '&password=' + this.password;
+
+    const body = new URLSearchParams();
+    Object.keys(value).forEach(key => {
+      body.set(key, value[key]);
+    });
+
+    this.http.post('http://107.170.225.6/TikTakPHP/customer/insert.php', 
+      body.toString(), 
+      {headers}
+    )
+      .map(res => res.json())
+      .subscribe(
+        data => {
+              this.loading = JSON.stringify(data);
+              this.result = data.message;
+              if(data.data == 1){
+                this.navCtrl.setRoot(VendorsListPage);
+              }else {
+                this.showRegFailed();
+              }
+          },
+          (err: HttpErrorResponse) => {
+                if (err.error instanceof Error) {
+                  // A client-side or network error occurred. Handle it accordingly.
+                  this.result = 'An error occurred:', err.error.message;
+                } else {
+                  // The backend returned an unsuccessful response code.
+                  // The response body may contain clues as to what went wrong,
+                  this.result = `Backend returned code ${err.status}, body was: ${err.error}`;
+                }
+            }
+        );
+  }
+ 
+  showRegFailed() {
+    let alert = this.alertCtrl.create({
+      title: 'Incorrect email or password!',
+      subTitle: 'Please Try Again' + this.email + ' ' + this.password,
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
--- src/pages/signup-vendor/signup-vendor.ts
+++ src/pages/signup-vendor/signup-vendor.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { ServicesListPage } from '../services-list/services-list';
+import { ClientsListPage } from '../clients-list/clients-list';
 
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
@@ -20,6 +21,12 @@ import { HttpErrorResponse } from '@angular/common/http';
   templateUrl: 'signup-vendor.html',
 })
 export class SignupVendorPage {
+  loading: string;
+  fname: string;
+  lname: string;
+  email: string;
+  password: string;
+  result: string;
   vendors: Array<{
     fname: string, 
     lname: string,
